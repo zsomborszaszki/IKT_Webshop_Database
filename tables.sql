0eb789168b775b57363fa80abcd08b6d86c17c8a
Börzsönyi Gergő 642 <borger642@logiker.hu>
2026-02-06 09:23:08 +0100

    táblák összekötve

--- tables.sql
+++ tables.sql
@@ -2,7 +2,8 @@ CREATE TABLE 'product' (
 'ID' int(11) NOT NULL,
 'name' varchar(255) NOT NULL,
 'price' int(11) NOT NULL,
-'quantity' int (11) NOT NULL
+'quantity' int (11) NOT NULL,
+PRIMARY KEY ('ID')
 )
 
 CREATE TABLE 'product_order'(
@@ -13,7 +14,10 @@ CREATE TABLE 'product_order'(
 CREATE TABLE 'payment'(
 'ID' int(11) NOT NULL,
 'order_ID' int(11) NOT NULL,
-'amount' int(11) NOT NULL
+'amount' int(11) NOT NULL,
+PRIMARY KEY ('ID'),
+UNIQUE ('order_ID'),
+FOREIGN KEY ('order_ID') REFERENCES 'order'('ID')
 )
     
 CREATE TABLE 'users'(
@@ -22,16 +26,21 @@ CREATE TABLE 'users'(
     'email' varchar(255) NOT NULL,
     'password' varchar(255) NOT NULL,
     'is_active' tinyint(1) NOT NULL DEFAULT 1,
-    'role_ID' int(11) DEFAULT NULL
+    'role_ID' int(11) DEFAULT NULL,
+    PRIMARY KEY ('ID'),
+    UNIQUE ('username', 'email')
 )
 CREATE TABLE 'role'(
     'ID' int(11) NOT NULL,
-    'role_name' varchar(255) NOT NULL
+    'role_name' varchar(255) NOT NULL,
+    PRIMARY KEY ('ID')
+)
 
 CREATE TABLE 'order'(
     'ID' int(11) NOT NULL,
     'delivery_location' varchar(255) NOT NULL,
     'user_ID' varchar(255) NOT NULL,
     'order_date' datetime NOT NULL,
-    'product' int(11) NOT NULL
+    'product' int(11) NOT NULL,
+    PRIMARY KEY ('ID')
 )
